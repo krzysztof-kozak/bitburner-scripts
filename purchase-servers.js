00a1d8f6ce8a0c118af2a5647494aa15cd413d4d
Krzysztof Kozak <hithard5@gmail.com>
2021-05-30 03:50:04 +0200

    Removed unnecesary code from purchase-servers.js

--- purchase-servers.js
+++ purchase-servers.js
@@ -35,20 +35,6 @@ export async function main(ns) {
       let purchasedServer = purchaseServer(`${name}-${ram}-${i}`, ram);
       tprint({ purchasedServer });
       tprint(`${name}-${ram}-${i}`);
-
-      scp(['/scripts/perma-weaken.js', '/scripts/perma-grow.js', '/scripts/perma-hack.js'], purchasedServer);
-
-      let serverMaxRam = getServerMaxRam(purchasedServer);
-      let growRamRequired = getScriptRam('/scripts/perma-grow.js');
-
-      exec('/scripts/perma-weaken.js', purchasedServer);
-      exec('/scripts/perma-hack.js', purchasedServer);
-
-      let serverUsedRam = getServerUsedRam(purchasedServer);
-      let serverFreeRam = serverMaxRam - serverUsedRam;
-      let threads = Math.floor(serverFreeRam / growRamRequired);
-
-      exec('/scripts/perma-grow.js', purchasedServer, threads, target);
     }
   }
 }
